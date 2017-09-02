747daa088c69aa963f71f2871b7f4cbe2b8d07ba
Fábio Carvalho <fabio.carvalllho@live.com>
2017-09-02 15:45:16 -0300

    Database name changed

--- SMOS.Infra/Migrations/Configuration.cs
+++ SMOS.Infra/Migrations/Configuration.cs
@@ -5,14 +5,14 @@ namespace SMOS.Infra.Migrations
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<SMOS.Infra.Context.BalancaIOContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<Context.SMOSContext>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = true;
         }
 
-        protected override void Seed(SMOS.Infra.Context.BalancaIOContext context)
+        protected override void Seed(Context.SMOSContext context)
         {
             //  This method will be called after migrating to the latest version.
 
